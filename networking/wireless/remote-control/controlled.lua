cdd9423d93d835bbc06bab46a39f8016d05772bc
Davide Dalera <dvddlr10@gmail.com>
2022-04-19 13:09:35 +0200

    Update controlled.lua

--- networking/wireless/remote-control/controlled.lua
+++ networking/wireless/remote-control/controlled.lua
@@ -1,7 +1,15 @@
 local modem = peripheral.find("modem")
+local tArgs = {...}
+
+if #tArgs ~= 1 then
+    local program_name = arg[0] or fs.getName(shell.getRunningProgram())
+    print("Usage: "..program_name.." <controller_id>")
+    return
+end
+
 
 if not modem or not modem.isWireless() then
-    printError("An attched wireless modem is requierd.")
+    printError("An attached wireless modem is requierd.")
     return
 end
 

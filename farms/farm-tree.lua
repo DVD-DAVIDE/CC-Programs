41e3a740285c9d4e853096ccf30eb23a1c77128c
Davide Dalera <dvddlr10@gmail.com>
2023-09-09 11:00:21 +0200

    Added automatic refuelling
    Now the turtle automatically refuels itself using a stack of logs it keeps in the second slot.

--- farms/farm-tree.lua
+++ farms/farm-tree.lua
@@ -1,15 +1,33 @@
 --[[-
 A tree farm program. Place a chest under the turtle and run the program after giving it saplings.
 It'll place trees on all four sides, and turn to check if any is grown every 10 seconds.
-Note that the program doesn't take care of fuel in any way. If it's running low, terminate the program and refuel the turtle manually.
 ]]
 
+local MINFUEL = 100
+
+local id, fuel
+
+local function refuel()
+    os.setComputerLabel(("Turtle %d | Fuel: %d - Refuelling"):format(id, fuel))
+    turtle.select(2)
+    while turtle.getItemCount() < 16 do
+        os.setComputerLabel(("Turtle %d | Fuel: %d - Waiting for fuel"):format(id, fuel))
+        sleep(10)
+    end
+    turtle.refuel()
+    turtle.select(1)
+end
+
 local function cleanInv()
-    for i = 16, 2, -1 do
+    for i = 16, 3, -1 do
         turtle.select(i)
         if turtle.getItemCount() > 0 then
             if turtle.getItemDetail().name:match("_sapling") then
                 if not (turtle.transferTo(1) and turtle.transferTo(1)) then
+                    turtle.dropDown()
+                end
+            elseif turtle.getItemDetail().name:match("_log") then
+                if not (turtle.transferTo(2) and turtle.transferTo(2)) then
                     turtle.dropDown()
                 end
             else
@@ -45,14 +63,22 @@ local function chopDown()
     cleanInv()
 end
 
+id = os.getComputerID()
 
 while true do
-    os.setComputerLabel("Turtle "..os.getComputerID().." | Fuel: "..tostring(turtle.getFuelLevel()).." - Farming trees")
+    fuel = turtle.getFuelLevel()
+    if fuel < MINFUEL then
+        refuel()
+    end
+    
+    os.setComputerLabel(("Turtle %d | Fuel: %d - Farming trees"):format(id, fuel))
+    turtle.select(1)
 
     local isBlockFront, tBlockFront = turtle.inspect()
     if isBlockFront and tBlockFront.tags["minecraft:logs"] then
         chopDown()
     end
+
     if not turtle.detect() then
         turtle.place()
     end
